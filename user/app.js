fe1e3462fe9e817f0654ca51c15a3f7e6cf2852e
Rachel Huang <hsinjuhu@usc.edu>
2023-11-12 21:34:45 -0800

    Added email verification function

--- user/app.js
+++ user/app.js
@@ -1,21 +1,23 @@
 const express = require("express");
-require('./db')
+require("./db");
 const userRouter = require("./routes/user");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.use("/api/user", userRouter);
 
-app.post("/sign-in",
+app.post(
+  "/sign-in",
   (req, res, next) => {
-    const { email, password } = req.body
+    const { email, password } = req.body;
     if (!email || !password)
-      return res.json({ error: 'email/ password missing!' })
-    next()
+      return res.json({ error: "email/password missing!" });
+    next();
   },
   (req, res) => {
-    res.send("<h1>Hello I am from your backend about</h1>");
-  });
+    res.send("<h1>Hello I am about page</h1>");
+  }
+);
 
 app.listen(8000, () => {
   console.log("the port is listening on port 8000");
