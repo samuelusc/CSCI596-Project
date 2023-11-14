766ce10a29f453339fa9100e0752e330400b7360
Rachel Huang <hsinjuhu@usc.edu>
2023-11-13 16:21:24 -0800

    Added password reset function

--- user/middleware/validator.js
+++ user/middleware/validator.js
@@ -20,3 +20,13 @@ exports.validate = (req, res, next) => {
 
     next();
 };
+
+exports.validatePassword = [
+    check("newPassword")
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage("Password is missing!")
+        .isLength({ min: 8, max: 20 })
+        .withMessage("Password must be 8 to 20 characters long!"),
+];
--- user/routes/user.js
+++ user/routes/user.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const { create, verifyEmail, resendEmailVerificationToken } = require("../controllers/user");
-const { userValidtor, validate } = require("../middlewares/validator");
+const { create, verifyEmail, resendEmailVerificationToken, forgetPassword, sendResetPasswordTokenStatus, resetPassword } = require("../controllers/user");
+const { isValidPassResetToken } = require("../middlewares/user");
+const { userValidtor, validate, validatePassword } = require("../middlewares/validator");
 
 const router = express.Router();
 
 router.post("/create", userValidtor, validate, create);
 router.post("/verify-email", verifyEmail);
 router.post("/resend-email-verification-token", resendEmailVerificationToken);
+router.post('/forget-password', forgetPassword)
+router.post('/verify-pass-reset-token', isValidPassResetToken, sendResetPasswordTokenStatus)
+router.post('/reset-password', validatePassword, validate, isValidPassResetToken, resetPassword)
 
 module.exports = router;
