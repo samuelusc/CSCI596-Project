1cd41bdede601bdaf6a2346bea33f7f8c029f6b5
Guodong Sun <guodongs@usc.edu>
2023-12-04 16:58:51 -0800

    Use OMDB API to get movie detail

---
+++ OMDB_API.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+
+var title = 'Aquaman';
+var year = '2018';
+const url = encodeURI('http://www.omdbapi.com/?apikey=57e618fe&t=' + title + '&y=' + year + '&plot=full');
+
+fetch(url).then(res => res.json()).then(json => {
+    var movie = {};
+    movie.title = json.Title;
+    movie.description = json.Plot;
+    movie.poster = json.Poster;
+    movie.rating = json.imdbRating;
+    movie.director = json.Director;
+    movie.writer = json.Writer;
+    movie.cast = json.Actors;
+    movie.genre = json.Genre;
+    console.log(movie);
+})
