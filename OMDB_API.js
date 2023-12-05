aff6d34a91e5b07cf67e1025576028d89f7af066
Guodong Sun <guodongs@usc.edu>
2023-12-04 17:31:51 -0800

    Include released year information

--- OMDB_API.js
+++ OMDB_API.js
@@ -1,13 +1,14 @@
 const fetch = require('node-fetch');
 
 var title = 'Aquaman';
-var year = '2010';
+var year = '2018';
 const url = encodeURI('http://www.omdbapi.com/?apikey=57e618fe&t=' + title + '&y=' + year + '&plot=full');
 
 fetch(url).then(res => res.json()).then(json => {
     if (json.Response === 'True') {
         var movie = {};
         movie.title = json.Title;
+        movie.year = json.Year;
         movie.description = json.Plot;
         movie.poster = json.Poster;
         movie.rating = json.imdbRating;
