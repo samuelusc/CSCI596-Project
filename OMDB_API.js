691106272805ee38bbeaf8747589d81772723f17
Guodong Sun <guodongs@usc.edu>
2023-12-04 17:03:27 -0800

    Handle situation that the movie is not found

--- OMDB_API.js
+++ OMDB_API.js
@@ -1,18 +1,23 @@
 const fetch = require('node-fetch');
 
 var title = 'Aquaman';
-var year = '2018';
+var year = '2010';
 const url = encodeURI('http://www.omdbapi.com/?apikey=57e618fe&t=' + title + '&y=' + year + '&plot=full');
 
 fetch(url).then(res => res.json()).then(json => {
-    var movie = {};
-    movie.title = json.Title;
-    movie.description = json.Plot;
-    movie.poster = json.Poster;
-    movie.rating = json.imdbRating;
-    movie.director = json.Director;
-    movie.writer = json.Writer;
-    movie.cast = json.Actors;
-    movie.genre = json.Genre;
-    console.log(movie);
+    if (json.Response === 'True') {
+        var movie = {};
+        movie.title = json.Title;
+        movie.description = json.Plot;
+        movie.poster = json.Poster;
+        movie.rating = json.imdbRating;
+        movie.director = json.Director;
+        movie.writer = json.Writer;
+        movie.cast = json.Actors;
+        movie.genre = json.Genre;
+        console.log(movie);
+    }
+    else {
+        console.log(json.Error);
+    }
 })
