25daba65bb1e8cc2e5353e1e223682d7f4f76da6
Rachel Huang <hsinjuhu@usc.edu>
2023-12-06 13:58:48 -0800

    Added isAuth

--- user/app.js
+++ user/app.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const morgan = require("morgan");
+const { errorHandler } = require("./middlewares/error");
+const cors = require("cors");
 require("dotenv").config();
 require("./db");
+require("express-async-errors");
 const userRouter = require("./routes/user");
 
+
+
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use('/api/user', userRouter);
 
 
+app.use(errorHandler);
+ 
 
 app.post(
     "/sign-in", 
--- user/controllers/user.js
+++ user/controllers/user.js
@@ -76,8 +76,9 @@ exports.verifyEmail = async(req, res) => {
         subject: "Welcom Email",
         html: "<h1>Welcome to our app and thanks for choosing us.</h1>",
     });
-
-    res.json({message: "Your email is verified."})
+    
+    const jwtToken = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.json({user: { id: user._id, name: user.name, email: user.email, token: jwtToken }, message: "Your email is verified."})
 };
 
 exports.resendEmailVerificationToken = async (req, res) => {
